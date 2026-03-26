82d1cb865b251056cdff7e2bff66f33f966d690e
murgatt <12082276+murgatt@users.noreply.github.com>
2026-03-26 17:24:15 +0100

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({ component: App })
+
+function App() {
+  return (
+    <main className="gap flex min-h-screen flex-col justify-center text-center">
+      <h1 className="text-2xl font-semibold">
+        tanstack-start-template - A simple TanStack Start starter template
+      </h1>
+    </main>
+  )
+}
