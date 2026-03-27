8ba8502e4d1f0e5b7207d9fe3765864cbf01bfe0
murgatt <12082276+murgatt@users.noreply.github.com>
2026-03-28 00:09:10 +0100

    refactor: remove unnecessary class from main element in App component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export const Route = createFileRoute('/')({ component: App });
 
 function App() {
   return (
-    <main className="gap flex min-h-screen flex-col justify-center text-center">
+    <main className="flex min-h-screen flex-col justify-center text-center">
       <Title />
     </main>
   );
